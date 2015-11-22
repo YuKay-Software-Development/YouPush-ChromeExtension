eadfb4d61b600f5791d0b3449b9c9d7f6377e7da
TheKayneGame <Kayne.saridjo@gmail.com>
2015-11-22 15:03:20 +0100

    issue #2 added more debug stuff and still not connected to internet lel

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,32 +1,72 @@
 ﻿$(function () {
-    
-    $('#test').on('mouseenter', function (evt) {
+	
+	var title = "test";
+	var notifTimeout = $('[name=NotifTimeout]').val();
+	var notifTimeoutSec = notifTimeout * 1000
+	
+	var time = /(..)(:..)/.exec(new Date());     
+	var hour = time[1];
 
-        //$(evt.currentTarget).text("HOVER!!!11!elf");
+	if (Notification.permission != "granted") {
+		Notification.requestPermission();
+	}
 
-    });
+	$('#test').on('mouseenter', function (evt) {
 
-    $('#test').on('click', function (evt) {
+		//$(evt.currentTarget).text("HOVER!!!11!elf");
 
-        var chromeStuff = chrome.notifications;
+		});
 
-        var notifTimeout = $('[name=NotificationTimeout]').val() * 1000;
+	$('#test').on('click', function (evt) {
 
-        var title = "test";
-        var time = /(..)(:..)/.exec(new Date());     
-        var hour = time[1]; 
-        var message = "i liek weird pron";
-        var notif = new Notification(title + ' - ' + hour + time[2], {
-            icon: 'Logo-128x128.png',
-            body: message
-	    });
+		var notifMessage = $('[name=NotifMessage]').val();
 
-        console.log(notif);
+		window["notif"] = new Notification(title + ' - ' + hour + time[2], {
+			type: 'progress',
+			progress: 69,
+			icon: 'Logo-128x128.png',
+			body: notifMessage,
+			dir: 'rtl',
+			tag: '<h1>BAKA</h1>',
+			requireInteraction: true
+		});
+		
+		notif.onclose = function (evt) {
+			console.group("Closed");
+			console.log(evt);
+			console.groupEnd();
+		};
 
-        setTimeout(function () {
-            notif.close();
-        }, notifTimeout);
+		notif.onclick = function (evt) {
+			//var messageWindow = window.open("messagewindow.html", "", "width=400, height=500", resizable = false);
+			chrome.windows.create({ 'url': 'messagewindow.html', 'type': 'panel','width': 400, 'height': 500 }, function (window) {  });
+			console.group("Clicked");
+			console.log(evt);
+			console.groupEnd();
+		};
 
-    });
+		console.log(notif);
+
+		setTimeout(function () {
+			notif.close();
+		}, notifTimeoutSec);
+
+
+	});
+
+	$(document).ready(function () {
+
+		var sPath = window.location.pathname;
+		var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+		console.log(sPage);
+		if (sPage == "messagewindow.html") {
+
+			document.getElementById("pageTitle").innerHTML = title;
+			document.getElementById("title").innerHTML = "New text title!";
+			document.getElementById("message").innerHTML = "New text!";
+
+		}
+
+	});
 
 })
